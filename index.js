61fd7973a27ba84cc8eb5c19d3cc142812a1bc7e
Codi Kraus <codi.kraus@gmail.com>
2021-09-30 10:37:33 -0500

    commit tree 80a61ad7241ffcf47e62141e8fc0e8785d6b0c09

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+function map(array, callbackFn) {
+    const r = [];
+    for (const ele of array) {
+        r.push(callbackFn(ele))
+    }
+    return r;
+}
+
+function reduce(array, callbackFn, startingPt) {
+    let memo;
+    let indx;
+
+    if (!!startingPt) {
+        memo = startingPt;
+        indx = 0;
+    } else {
+        memo = array[0];
+        indx = 1;
+    }
+
+    for (indx; indx < array.length; indx++) {
+        memo = callbackFn(memo, array[indx]);
+    }
+    return memo;
+}
